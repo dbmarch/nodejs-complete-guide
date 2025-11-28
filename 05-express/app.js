2d9a4a8b7d7ad60df298e5253395973f4eb2c912
Dave March <dbmarch@gmail.com>
2025-11-27 23:14:28 -0500

    serve pages

--- 05-express/app.js
+++ 05-express/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -8,12 +8,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
