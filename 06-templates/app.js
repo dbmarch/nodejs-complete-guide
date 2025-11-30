4fbccda211ecbe152295cd6e9677f2ee4c03739e
Dave March <dbmarch@gmail.com>
2025-11-29 22:34:28 -0500

    clean up ejs files

--- 06-templates/app.js
+++ 06-templates/app.js
@@ -29,7 +29,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render('404', { docTitle: 'Page Not Found' });
+    res.render('404', { path: '', docTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
