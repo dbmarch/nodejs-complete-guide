61b643d95330a829584941853a72222e79578b45
Dave March <dbmarch@gmail.com>
2025-11-29 20:22:06 -0500

    demo pug

--- 06-templates/routes/shop.js
+++ 06-templates/routes/shop.js
@@ -4,12 +4,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 const adminData = require('./admin');
-
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
