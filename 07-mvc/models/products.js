c8e8e040dd8462bda583503875ec00216aebe8b7
Dave March <dbmarch@gmail.com>
2025-11-30 00:29:16 -0500

    Refactor

--- 07-mvc/models/products.js
+++ 07-mvc/models/products.js
@@ -1,52 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = class Product {
 
+getDatabasePath = ()=>path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+getProductsfromFile = (cb) => {
+   fs.readFile(getDatabasePath(), (err, fileContent) => {
+      if (err) {
+         return cb([]);
+      }
+      cb(JSON.parse(fileContent));
+   });
+};
+
+module.exports = class Product {
    constructor(title) {
      this.title = title;
    }
 
    save() {
-      let products = [];
-      const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-      
-      if (fs.existsSync(p)) {
-            fs.readFile(p, (err, fileContent) => {
-               if (err) {
-                  console.log ('read error', err);
-               }else{
-                  products = JSON.parse(fileContent);
-                  products = [...products, this];
-                  fs.writeFile(p, JSON.stringify(products), (err) => {
-                     console.log('write error', err);
-                  });
-               }
-            })
-      } else {
-
-         console.log ('file does not exist');
+      getProductsfromFile((products) => { 
          products = [...products, this];
-         fs.writeFile(p, JSON.stringify(products), (err) => {
+         fs.writeFile(getDatabasePath(), JSON.stringify(products), (err) => {
             console.log('write error', err);
-         });         
-      }
+         });
+      })
    }
    
    static fetchAll(cb) {
-      const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-      if (fs.existsSync(p)) {
-         fs.readFile(p, (err, fileContent) => {
-            if (err) {
-               console.log ('error', err);
-               return cb([]);
-            }
-            let products = JSON.parse(fileContent);
-            console.log('products', products);
-            return cb(products);
-         });
-      } else {
-         console.log ('file does not exist');
-      }
-   }
+      getProductsfromFile((products) => cb(products));
+   };
+      
 };
